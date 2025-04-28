c559bcf2bc4ce207188c729a254654f6e667a341
Sophia Thapa <sophia.thapa55@gmail.com>
2025-04-28 21:05:30 +0545

    Todo-list

---
+++ learn-java/Todo-list/scripts/todo.js
@@ -0,0 +1,49 @@
+const todoList = [
+  { name: "exercise", dueDate: "2025-12-01" },
+  { name: "wash dishes", dueDate: "2023-11-25" },
+];
+
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    console.log(todoObject);
+    // const name = todoObject.name;
+    // const dueDate= todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    // console.log(dueDate);
+
+    const html = `<div>${name}</div>
+    <div> ${dueDate}</div> <button class="delete-button"
+    onclick= "
+    todoList.splice(${i},1);
+    renderTodoList();
+    ">
+    Delete</button></li> `; //generating the html
+    todoListHTML += html;
+  }
+
+  // console.log(todoListHTML);
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+
+renderTodoList();
+
+function addTodo() {
+  const inputElement = document.querySelector("input");
+  const name = inputElement.value;
+  const dueDateElement = document.querySelector(".js-due-date-button");
+  const dueDate = dueDateElement.value;
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    //or
+    name,
+    dueDate,
+  });
+  inputElement.value = "";
+  dueDateElement.value = "";
+  console.log(todoList);
+  renderTodoList();
+}
